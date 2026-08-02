a484b6d0c4970978f523899be4fb2e444b503343
Arya Palanivel <80607717+arya-palanivel@users.noreply.github.com>
2026-08-01 19:39:05 -0700

    Add: minimum-window-with-characters - submission-6

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-6.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        check = set(t)
+        have, need = 0, len(check)
+        left = 0
+        resTuple = (0, 0)
+        shortest = 0
+        windowDict = dict()
+
+        for i in range(len(s)):
+            if s[i] in check:
+                windowDict[s[i]] = windowDict.get(s[i], 0) + 1
+                if windowDict[s[i]] == 1:
+                    have += 1
+
+
+            while have == need:
+
+                if i-left+1 < shortest:
+                    shortest = i-left+1
+                    resTuple = (left, i)
+
+                if s[left] in check:
+                    windowDict[s[left]] -= 1
+                    if windowDict[s[left]] == 0:
+                        have -=1
+                
+                left+=1
+        if shortest == 0:
+            return ""
+        return s[resTuple[0]:resTuple[1]+1]
